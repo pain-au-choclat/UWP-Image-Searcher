198931cf44f9c57c2e1b6f4762a99dd8a1881e8e
Pain-au-chocllat <samuel18081988@hotmail.co.uk>
2019-05-10 23:10:11 +0100

    Cognitive search

--- UWP-Image-Searcher/MainPage.xaml.cs
+++ UWP-Image-Searcher/MainPage.xaml.cs
@@ -1,30 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Net;
+using Windows.Data.Json;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace UWP_Image_Searcher
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        const string SubscriptionKey = "put your key here!!!";
+        const string UriBase = "https://api.cognitive.microsoft.com/bing/v7.0/images/search";
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        private void OnKeyDownHandler(object sender, KeyRoutedEventArgs e)
+        {
+
+            if (e.Key == Windows.System.VirtualKey.Enter &&
+                    searchTermsTextBox.Text.Trim().Length > 0)
+            {
+                string imageUrl = FindUrlOfImage(searchTermsTextBox.Text);
+
+                foundObjectImage.Source = new BitmapImage(new Uri(imageUrl, UriKind.Absolute));
+
+            }
+        }
+
+        struct SearchResult
+        {
+            public String jsonResult;
+            public Dictionary<String, String> relevantHeaders;
+        }
+
+        private string FindUrlOfImage(string targetString)
+        {
+            SearchResult result = PerformBingImageSearch(targetString);
+
+            JsonObject jsonObj = JsonObject.Parse(result.jsonResult);
+            JsonArray results = jsonObj.GetNamedArray("value");
+            if (results.Count > 0)
+            {
+                JsonObject first_result = results.GetObjectAt(0);
+                String imageUrl = first_result.GetNamedString("contentUrl");
+                return imageUrl;
+            }
+            else
+                return "https://docs.microsoft.com/learn/windows/build-internet-connected-windows10-apps/media/imagenotfound.png";
+        }
+
+        static SearchResult PerformBingImageSearch(string searchTerms)
+        {
+            string uriQuery = UriBase + "?q=" + Uri.EscapeDataString(searchTerms);
+            WebRequest request = WebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = SubscriptionKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            var searchResult = new SearchResult()
+            {
+                jsonResult = json,
+                relevantHeaders = new Dictionary<String, String>()
+            };
+
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+
+            return searchResult;
+        }
+
     }
 }
